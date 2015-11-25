df136aed96ef7d6a4d1cfa630d716243bd51b768
hopptramp <rob-hopps@hotmail.co.uk>
2015-11-25 18:55:26 +0000

    missed scripts

---
+++ Frogger Clone/Assets/Scripts/Spawning/Spawner.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour 
+{
+	// input parameters to define what spawns
+	public GameObject[] objectToSpawn;
+	public int spawnWhat; // the prefab in the array must match the case number in the switch statement (with a minus 1)
+	
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+
+	void Awake()
+	{
+		spawnObject ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void spawnObject()
+	{
+		switch (spawnWhat) 
+		{
+		case 1: // platform
+			GameObject platform;
+			//instantiate the object from a prefab
+			platform = Instantiate(objectToSpawn[spawnWhat - 1], gameObject.transform.position, Quaternion.identity) as GameObject;
+			// assign the parameters by passing through a struct from the prefab
+			logStructs settings = objectToSpawn[spawnWhat - 1].GetComponent<logStructs>();
+			platform.GetComponent<baseObject>().assignParameters(settings);
+			break;
+		case 2: // enemy
+
+			break;
+		case 3: // something else?
+
+			break;
+		}
+	
+	}
+}
---
+++ Frogger Clone/Assets/Scripts/Spawning/baseObject.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class baseObject : MonoBehaviour {
+
+	float speedx;
+	float speedz;
+	
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Boundary") 
+		{
+			Destroy(this);
+		}
+		
+	}
+
+	public void assignParameters(logStructs _struct)
+	{
+		speedx = _struct.speedX;
+		speedz = _struct.speedY;
+		
+		transform.localScale = new Vector3 (_struct.sizeX, _struct.sizeY, 1 );
+	}
+
+	void FixedUpdate() 
+	{
+		transform.Translate ( speedx * Time.deltaTime , 0 , 0);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
