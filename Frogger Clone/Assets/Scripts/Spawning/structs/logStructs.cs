8b5b19fcb15020cf5f35cfb8321fea8da0f8e145
hopptramp <rob-hopps@hotmail.co.uk>
2015-11-25 18:50:01 +0000

    basic object stuff

---
+++ Frogger Clone/Assets/Scripts/Spawning/structs/logStructs.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class logStructs : MonoBehaviour {
+
+	public float sizeX = 5;
+	public float sizeY = 2;
+	public float speedX = 2;
+	public float speedY = 0;
+
+}
