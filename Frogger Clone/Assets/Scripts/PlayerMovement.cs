fd0fa389409e5aabf67cd00859c01ccc9a58ab4c
xavboy <aldrodgex@gmail.com>
2015-11-25 19:38:32 +0000

    Player Tweek for Organization
    just moved stuff around

--- Frogger Clone/Assets/Scripts/PlayerMovement.cs
+++ Frogger Clone/Assets/Scripts/PlayerMovement.cs
@@ -49,78 +49,16 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	}
-	void Direction (float _inputHor, float _inputVert)
-	{
-		//checks if moving in any direction
-		if ((_inputHor != 0) || (_inputVert != 0)) {
-			moving = true;
-		} 
-		else {
-			moving = false;
-		}
-
-		//checks if moving vertically
-		if (_inputVert != 0) {
-			movingVertically = true;
-			movingHorizontally = false;
-
-			//check if moving up
-				if(_inputVert > 0) {
-
-				facingLeft = false;
-				facingRight = false;
-				facingDown = false;
-				facingUp = true;
-			}
-			//check if moving up
-			if(_inputVert < 0) {
-
-				facingLeft = false;
-				facingRight = false;
-				facingDown = true;
-				facingUp = false;
-			}
-		} 
-		//not moving vertically
-		else {
-			movingVertically = false;
-		}
-
-		//checks if moving horizontally
-		if (_inputHor != 0) {
-			
-			movingHorizontally = true;
-			movingVertically = false;
-			//checks if moving right
-			if(_inputHor > 0) {
-				
-				facingLeft = false;
-				facingRight = true;
-				facingDown = false;
-				facingUp = false;
-			}
-			//checks if moving Left
-			if(_inputHor < 0) {
-				
-				facingLeft = true;
-				facingRight = false;
-				facingDown = false;
-				facingUp = false;
-	
-			}
-		}
-		//not moving horizontally
-		else {
-			movingHorizontally = false;
-		}
-	}
-
 	void Movement (float _inputHor, float _inputVert) {
 
-		if (movingHorizontally == true) {
+		//if (movingHorizontally == true)
+		if ((movingHorizontally == true) && (movingVertically == false)) 
+		{
 			transform.Translate (movementSpeed * _inputHor * Time.deltaTime, 0.0f, 0.0f);
 		}
-		if (movingVertically == true) {
+		//if (movingVertically == true)
+		if ((movingHorizontally == false) && (movingVertically == true)) 
+		{
 			transform.Translate (0.0f, movementSpeed * _inputVert * Time.deltaTime, 0.0f);
 		}
 
@@ -169,6 +107,72 @@ public class PlayerMovement : MonoBehaviour {
 				
 				_anim.SetTrigger ("moveDown");
 			}
+		}
+	}
+
+	void Direction (float _inputHor, float _inputVert)
+	{
+		//checks if moving in any direction
+		if ((_inputHor != 0) || (_inputVert != 0)) {
+			moving = true;
+		} 
+		else {
+			moving = false;
+		}
+		
+		//checks if moving vertically
+		if (_inputVert != 0) {
+			movingVertically = true;
+			//movingHorizontally = false;
+			
+			//check if moving up
+			if(_inputVert > 0) {
+				
+				facingLeft = false;
+				facingRight = false;
+				facingDown = false;
+				facingUp = true;
+			}
+			//check if moving up
+			if(_inputVert < 0) {
+				
+				facingLeft = false;
+				facingRight = false;
+				facingDown = true;
+				facingUp = false;
+			}
+		} 
+		//not moving vertically
+		else {
+			movingVertically = false;
+		}
+		
+		//checks if moving horizontally
+		if (_inputHor != 0) {
+			
+			movingHorizontally = true;
+			//movingVertically = false;
+			//checks if moving right
+			if(_inputHor > 0) {
+				
+				facingLeft = false;
+				facingRight = true;
+				facingDown = false;
+				facingUp = false;
+			}
+			//checks if moving Left
+			if(_inputHor < 0) {
+				
+				facingLeft = true;
+				facingRight = false;
+				facingDown = false;
+				facingUp = false;
+				
+			}
+		}
+		//not moving horizontally
+		else {
+			movingHorizontally = false;
 		}
 	}
 }
